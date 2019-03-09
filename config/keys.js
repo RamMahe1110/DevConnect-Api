31dc0be46a200efaad09c94e3321aae13363a05f
Ram Maheshwari <ramdevukd@gmail.com>
2019-03-09 23:08:35 +0530

    First Commit

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://ram123:ram123@cluster0-ddtop.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://ram123:ram123@cluster0-ddtop.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
 
 //secretOrKey: "SECRET"
--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
 
-//DB Config
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-//Connect to mongoDB
-mongoose.connect(db, { useNewUrlParser: true }).then(() => {
-  console.log("MongoDB Connected").catch(err => console.log(err));
-});
+// Connect to mongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Home"));
+// Passport middleware
+app.use(passport.initialize());
 
-//user Routes
+// passport config
+require("./config/passport")(passport);
+
+// user Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
